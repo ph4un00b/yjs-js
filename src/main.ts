52f12c3a7ab5a19666e11104a037cdf6435610d7
ph4un00b <1057021+ph4un00b@users.noreply.github.com>
2023-09-12 14:28:33 -0600

    +quill from npm

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,25 @@
 import './style.css';
 
+import Quill from 'quill';
 import QuillCursors from 'quill-cursors';
 
-console.log(QuillCursors);
+Quill.register('modules/cursors', QuillCursors);
+
+const quill = new Quill(document.querySelector('#editor'), {
+  modules: {
+    cursors: true,
+    toolbar: [
+      // adding some basic Quill content features
+      [{ header: [1, 2, false] }],
+      ['bold', 'italic', 'underline'],
+      ['image', 'code-block'],
+    ],
+    history: {
+      // Local undo shouldn't undo changes
+      // from remote users
+      userOnly: true,
+    },
+  },
+  placeholder: 'Start collaborating...',
+  theme: 'snow', // 'bubble' is also great
+});
